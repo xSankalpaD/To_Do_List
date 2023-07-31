9292cd598a365bfd9e5225c8cb8aaf8035c73a56
Sankalpa <90166191+xSankalpaD@users.noreply.github.com>
2023-07-31 00:22:33 -0400

    edit on delete button

--- script.js
+++ script.js
@@ -21,7 +21,13 @@ function createTaskItem(taskText) {
         
     // Adding delete button
     const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-btn"); //Add the custom CSS class to the button to edit it
+    
+    // Create an image element and set the image source
+    const deleteImage = document.createElement("img");
+    deleteImage.src = "delete.png"; // path to image
+
+    deleteButton.appendChild(deleteImage); // Append the image to the button
     deleteButton.addEventListener("click", function (){
         taskItem.remove();
     }); 
