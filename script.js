872d67cf2e180dd14bd4d6573c6728a53e089980
Sankalpa <90166191+xSankalpaD@users.noreply.github.com>
2023-08-07 00:38:51 -0400

    patch

--- script.js
+++ script.js
@@ -18,16 +18,14 @@ function createTaskItem(taskText) {
 
     const taskItem= document.createElement("li");
     taskItem.textContent = taskText;
-
-    
-        
+  
     // Adding delete button
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("delete-btn"); //Add the custom CSS class to the button to edit it
     
     // Create an image element and set the image source
     const deleteImage = document.createElement("img");
-    deleteImage.src = "delete.png"; // path to image
+    deleteImage.src = "images/delete.png"; // path to image
 
     deleteButton.appendChild(deleteImage); // Append the image to the button
     deleteButton.addEventListener("click", function (){
@@ -38,7 +36,6 @@ function createTaskItem(taskText) {
 
     return taskItem;
 }
-
 }
 
 taskList.addEventListener("click", function(e){
